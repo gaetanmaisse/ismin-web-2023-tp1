05d90745f292dca4e0cf0e36e79564c0d4b54777
Gaëtan Maisse <gaetanmaisse@gmail.com>
2023-07-30 17:26:19 +0200

    feat: create Book and Bookshelf classes

---
+++ src/Book.ts
@@ -0,0 +1,25 @@
+export interface Book {
+  /**
+   * International Standard Book Number of the book
+   */
+  isbn: string;
+
+  /**
+   * Title of the book
+   */
+  title: string;
+
+  /**
+   * Author of the book
+   */
+  author: string;
+
+  /**
+   * Date of publication of the book
+   */
+  date: string;
+}
+
+export const compareWithTitle = (a: Book, b: Book): number => {
+  return a.title.localeCompare(b.title);
+};
---
+++ src/Bookshelf.ts
@@ -0,0 +1,31 @@
+import { Book, compareWithTitle } from './Book';
+
+export class Bookshelf {
+  private readonly storage = new Map<string, Book>();
+
+  addBook(book: Book) {
+    this.storage.set(book.isbn, book);
+  }
+
+  getBook(isbn: string): Book {
+    const book = this.storage.get(isbn);
+    if (!book) {
+      throw new Error(`Book with ISBN ${isbn} not found`);
+    }
+    return book;
+  }
+
+  getAllBooks(): Array<Book> {
+    return Array.from(this.storage.values()).sort(compareWithTitle);
+  }
+
+  getBooksOf(author: string): Array<Book> {
+    return this.getAllBooks()
+      .filter((book) => book.author === author)
+      .sort(compareWithTitle);
+  }
+
+  getTotalNumberOfBooks(): number {
+    return this.storage.size;
+  }
+}
